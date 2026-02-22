4ea7383e0eac0e6721572a24d2c22c3a4f693d84
Sithum-Jayasinghe <abishekjayasinghe944@gmail.com>
2026-02-22 22:15:50 +0530

    Setup endpoints

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -4,10 +4,10 @@ import * as controller from '../controllers/feedbackController.js';
 
 const router = express.Router();
 
-router.post('/', controller.createFeedback);
-router.get('/', controller.getFeedbacks);
-router.get('/:id', controller.getFeedbackById);
-router.put('/:id', controller.updateFeedback);
-router.delete('/:id', controller.deleteFeedback);
+router.post('/createFeedback', controller.createFeedback);
+router.get('/getFeedbacks', controller.getFeedbacks);
+router.get('/getFeedback/:id', controller.getFeedbackById);
+router.put('/updateFeedback/:id', controller.updateFeedback);
+router.delete('/deleteFeedback/:id', controller.deleteFeedback);
 
 export default router;
