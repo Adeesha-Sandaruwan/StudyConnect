86599b9f41fef048658845b8279307fc0e0dd9e8
De Silva Y A S <it23551084@my.sliit.lk>
2026-03-08 15:10:58 +0530

    feat: implement forgot and reset password functionality with email notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
     return (
@@ -7,6 +9,8 @@ function App() {
             <Route path="/" element={<div>StudyConnect Home Feed Placeholder</div>} />
             <Route path="/login" element={<AuthPage />} />
             <Route path="/register" element={<AuthPage />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/resetpassword/:resettoken" element={<ResetPassword />} />
         </Routes>
     );
 }
